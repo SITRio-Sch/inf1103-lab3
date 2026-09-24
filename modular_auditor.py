db29ab6b7cec7100bc5b376404c1feacf6d7390a
rio <derpdiepie8523@gmail.com>
2026-09-24 18:11:19 +0800

    modular_auditor.py completed

--- modular_auditor.py
+++ modular_auditor.py
@@ -3,8 +3,12 @@ def get_valid_input():
 
     if user_input.isdigit() and (int(user_input) >= 0):
         return int(user_input)
+    elif user_input.lower() == "quit":
+        return user_input.lower()
     else:
+        user_input = "Input Error"
         print("Invalid Value")
+        return user_input
 
 def process_delivery(current_total, new_value): 
     current_total =+ new_value
@@ -15,3 +19,23 @@ def calculate_tax(amount):
 
 def generate_report(total_units, failed_attempts):
     print(f"Total Units Processed: {total_units}\nNumber of Failed/Rejected Entries: {failed_attempts}")
+
+
+inventory = 0 
+invalid_entries = 0
+
+while True:
+    user_input = get_valid_input()
+
+    if user_input == "Input Error":
+        invalid_entries += 1
+        continue
+    elif user_input == "quit":
+        generate_report(inventory, invalid_entries)
+        print(f"Tax: {calculate_tax(inventory)}")
+        break
+    else:
+        inventory += user_input
+        if inventory > 500:
+                print("Error! Total inventory exceeds 500 units!")
+                break
